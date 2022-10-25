4b6fc3a1747f8dba73940f8a3ff96de16b42abfb
Bradley Setzler <bradley.setzler@gmail.com>
2022-10-25 09:13:37 -0400

    Initial commit

---
+++ R/DiD_estimator.R
@@ -0,0 +1,191 @@
+
+#' Estimate DiD estimation for a single cohort (g) and a single event time (e).
+#' 
+#' @param inputdata A data.table.
+#' @param varname_options A list of the form varname_options = list(id_var, time_var, outcome_var, cohort_var), where all four arguments of the list must be a character that corresponds to a variable name in inputdata.
+#' @param control_group There are three possibilities: control_group="never-treated" uses the never-treated control group only; control_group="future-treated" uses those units that will receive treatment in the future as the control group; and control_group="all" uses both the never-treated and the future-treated in the control group. Default is control_group="all".
+#' @param event_baseperiod This is the base pre-period that is normalized to zero in the DiD estimation. Default is event_baseperiod=-1.
+#' @param min_event This is the minimum event time (e) to estimate. Default is NULL, in which case, no minimum is imposed.
+#' @param max_event This is the maximum event time (e) to estimate. Default is NULL, in which case, no maximum is imposed.
+#' @returns A list with two components: results_cohort is a data.table with the DiDge estimates (by event e and cohort g), and results_average is a data.table with the DiDe estimates (by event e, average across cohorts g).
+#' @examples
+#' varname_options = list(id_var = "id", time_var = "year", outcome_var = "price", cohort_var = "cohort")
+#' DiD(inputdata=simdata, varname_options=varname_options, control_group = "future-treated", event_baseperiod=-2, min_event=-3, max_event=5)
+#' @export
+DiDge <- function(inputdata, varname_options, cohort_time, event_baseperiod, event_postperiod, control_group = "all"){
+  
+  # set up variable names
+  time_var = varname_options$time_var
+  outcome_var = varname_options$outcome_var
+  cohort_var = varname_options$cohort_var
+  id_var = varname_options$id_var
+  
+  # prepare time periods
+  pre_time = cohort_time + event_baseperiod
+  post_time = cohort_time + event_postperiod
+  time_set = sort(inputdata[get(cohort_var) == cohort_time, unique(get(time_var))])
+  if(!(pre_time %in% time_set)){
+    stop(print(sprintf("error: for cohort %s, preperiod %s is unavailable.",cohort_time,event_baseperiod)))
+  }
+  if(!(post_time %in% time_set)){
+    stop(print(sprintf("error: for cohort %s, postperiod %s is unavailable.",cohort_time,event_postperiod)))
+  }
+  
+  # restrict to pre and post time periods of interest, then restrict to observations present in both time periods
+  inputdata = inputdata[get(time_var)==pre_time | get(time_var)==post_time]  
+  inputdata[, nobs := .N, by=id_var]
+  inputdata = inputdata[nobs==2 | (get(time_var)==pre_time & get(time_var)==post_time)]
+  gc()
+  
+  # define treated data and get means
+  treated_data_prepost = merge(
+    inputdata[(get(cohort_var) == cohort_time) & (get(time_var) == pre_time), list(tmp_idvar=get(id_var),treated_pre_outcome=get(outcome_var))],
+    inputdata[(get(cohort_var) == cohort_time) & (get(time_var) == post_time), list(tmp_idvar=get(id_var),treated_post_outcome=get(outcome_var))],
+    by=c("tmp_idvar")
+  )
+  treated_pre = treated_data_prepost[, list(Ntreated_pre=sum(!is.na(treated_pre_outcome)),Etreated_pre=mean(treated_pre_outcome))]
+  treated_post = treated_data_prepost[, list(Ntreated_post=sum(!is.na(treated_post_outcome)),Etreated_post=mean(treated_post_outcome),Etreated_var=var(treated_post_outcome))]
+  treated_results = cbind(treated_pre,treated_post)
+  treated_results$treated_diff_var = treated_data_prepost[, var(treated_post_outcome - treated_pre_outcome)]
+  
+  # define control data and get means
+  control_data_prepost = NULL
+  if(control_group == "all"){
+    control_data_prepost = merge(
+      inputdata[(get(cohort_var) > (cohort_time +  post_time)) & (get(time_var) == pre_time), list(tmp_idvar=get(id_var),control_pre_outcome=get(outcome_var))],
+      inputdata[(get(cohort_var) > (cohort_time +  post_time)) & (get(time_var) == post_time), list(tmp_idvar=get(id_var),control_post_outcome=get(outcome_var))],
+      by=c("tmp_idvar")
+    )
+  }
+  if(control_group == "never-treated"){
+    control_data_prepost = merge(
+      inputdata[(get(cohort_var) > (cohort_time +  post_time)) & is.infinite(get(cohort_var)) & (get(time_var) == pre_time), list(tmp_idvar=get(id_var),control_pre_outcome=get(outcome_var))],
+      inputdata[(get(cohort_var) > (cohort_time +  post_time)) & is.infinite(get(cohort_var)) & (get(time_var) == post_time), list(tmp_idvar=get(id_var),control_post_outcome=get(outcome_var))],
+      by=c("tmp_idvar")
+    )
+  }
+  if(control_group == "future-treated"){
+    control_data_prepost = merge(
+      inputdata[(get(cohort_var) > (cohort_time +  post_time)) & is.finite(get(cohort_var)) & (get(time_var) == pre_time), list(tmp_idvar=get(id_var),control_pre_outcome=get(outcome_var))],
+      inputdata[(get(cohort_var) > (cohort_time +  post_time)) & is.finite(get(cohort_var)) & (get(time_var) == post_time), list(tmp_idvar=get(id_var),control_post_outcome=get(outcome_var))],
+      by=c("tmp_idvar")
+    )
+  }
+  control_pre = control_data_prepost[, list(Ncontrol_pre=sum(!is.na(control_pre_outcome)),Econtrol_pre=mean(control_pre_outcome))]
+  control_post = control_data_prepost[, list(Ncontrol_post=sum(!is.na(control_post_outcome)),Econtrol_post=mean(control_post_outcome),Econtrol_var=var(control_post_outcome))]
+  control_results = cbind(control_pre,control_post)
+  control_results$control_diff_var = control_data_prepost[, var(control_post_outcome - control_pre_outcome)] 
+  
+  # combine means into an output table 
+  results = cbind(control_results,treated_results)
+  results[, Cohort := cohort_time]
+  results[, Preperiod := pre_time]
+  results[, CalendarTime := post_time]
+  results[, Preevent := event_baseperiod]
+  results[, EventTime := event_postperiod]
+  results[, Econtrol_SE := sqrt(Econtrol_var/Ncontrol_post)]
+  results[, Etreated_SE := sqrt(Etreated_var/Ntreated_post)]
+  results[, pred_Etreated_post := Etreated_pre + (Econtrol_post - Econtrol_pre)]
+  results[, ATTge := (Etreated_post - pred_Etreated_post)]
+  results[, ATTge_SE := sqrt(treated_diff_var/Ntreated_post + control_diff_var/Ncontrol_post)]
+  return(results)
+}
+
+
+
+DiDe <- function(inputdata, varname_options, control_group = "all", event_baseperiod=-1, min_event=NULL, max_event=NULL){
+  
+  # set up variable names
+  time_var = varname_options$time_var
+  outcome_var = varname_options$outcome_var
+  cohort_var = varname_options$cohort_var
+  id_var = varname_options$id_var
+  
+  # set up and checks
+  cohorts = sort(inputdata[, unique(get(cohort_var))])
+  nevertreated_exist = sum(is.infinite(cohorts))>0
+  cohorts = cohorts[!is.infinite(cohorts)]
+  
+  if(control_group=="never-treated"){
+    if(!nevertreated_exist){
+      stop("You specified control_group='never-treated', but there are no never-treated observations. Note: you must code never-treated observations as infinity (Inf).")
+    }
+  }
+  
+  if(control_group=="future-treated"){
+    if(length(cohorts) <= 1){
+      stop("You specified control_group='future-treated', but there is only one treatment cohort with finite cohort time, so no control units are available based on will-be-treated observations.")
+    }
+    cohorts = cohorts[!(cohorts == max(cohorts))]
+  }
+  
+  min_time_actual = inputdata[,min(get(time_var))]
+  invalid_cohorts = cohorts - event_baseperiod < min_time_actual
+  if(sum(invalid_cohorts) > 0){
+    print(sprintf("We cannot provide ATT estimates for cohort %s due to the absence of a base_preperiod in the data.", paste0(cohorts[invalid_cohorts], collapse=",")))
+    cohorts_todrop = cohorts[invalid_cohorts]
+    cohorts = cohorts[!invalid_cohorts]
+    inputdata = inputdata[!(get(cohort_var) %in% cohorts_todrop)]
+  }
+  
+  # cohort-specific estimation
+  results_cohort = data.table()
+  for(cc in cohorts){
+    # set up cohort-specific event times
+    times_for_cohort = sort(inputdata[get(cohort_var) == cc, unique(get(time_var))])
+    event_periods = times_for_cohort - cc
+    if(!is.null(min_event)){
+      event_periods = event_periods[event_periods >= min_event]
+    }
+    if(!is.null(max_event)){
+      event_periods = event_periods[event_periods <= max_event]
+    }
+    # loop DiD over the event times for this cohort
+    for(event_postperiod in event_periods){
+      res = DiDge(inputdata, cohort_time = cc, event_baseperiod = event_baseperiod, event_postperiod = event_postperiod, 
+                            varname_options=varname_options, control_group = control_group)
+      results_cohort = rbindlist(list(results_cohort,res))
+    }
+  }
+  results_cohort=results_cohort[order(Cohort,EventTime)]
+  
+  # take the average across cohorts
+  results_cohort[, Ntreated_event := sum(Ntreated_post), by="EventTime"]
+  results_cohort[, cohort_weights := Ntreated_post/Ntreated_event]
+  results_average = results_cohort[, list(ATTe=sum(cohort_weights * ATTge), 
+                                          ATTe_SE=sqrt(sum(cohort_weights^2 * ATTge_SE^2)),
+                                          Etreated_post=sum(cohort_weights*Etreated_post),
+                                          Etreated_pre=sum(cohort_weights*Etreated_pre),
+                                          Etreated_SE=sqrt(sum(cohort_weights^2*Etreated_SE^2)),
+                                          Econtrol_post=sum(cohort_weights*Econtrol_post),
+                                          Econtrol_pre=sum(cohort_weights*Econtrol_pre),
+                                          Ntreated_post=sum(Ntreated_post),
+                                          Ntreated_pre=sum(Ntreated_pre),
+                                          Ncontrol_post=sum(Ncontrol_post),
+                                          Ncontrol_pre=sum(Ncontrol_pre)
+                                          ), list(EventTime,Preevent)][order(EventTime,Preevent)]
+  
+  return(list(results_cohort=results_cohort, results_average=results_average))
+}
+
+#' Estimate DiD for all possible cohorts and event time pairs (g,e), as well as the average across cohorts for each event time (e).
+#' 
+#' @param inputdata A data.table.
+#' @param varname_options A list of the form varname_options = list(id_var, time_var, outcome_var, cohort_var), where all four arguments of the list must be a character that corresponds to a variable name in inputdata.
+#' @param control_group There are three possibilities: control_group="never-treated" uses the never-treated control group only; control_group="future-treated" uses those units that will receive treatment in the future as the control group; and control_group="all" uses both the never-treated and the future-treated in the control group. Default is control_group="all".
+#' @param event_baseperiod This is the base pre-period that is normalized to zero in the DiD estimation. Default is event_baseperiod=-1.
+#' @param min_event This is the minimum event time (e) to estimate. Default is NULL, in which case, no minimum is imposed.
+#' @param max_event This is the maximum event time (e) to estimate. Default is NULL, in which case, no maximum is imposed.
+#' @returns A list with two components: results_cohort is a data.table with the DiDge estimates (by event e and cohort g), and results_average is a data.table with the DiDe estimates (by event e, average across cohorts g).
+#' @examples
+#' varname_options = list(id_var = "id", time_var = "year", outcome_var = "price", cohort_var = "cohort")
+#' DiD(inputdata=simdata, varname_options=varname_options, control_group = "future-treated", event_baseperiod=-2, min_event=-3, max_event=5)
+#' @export
+DiD <- function(inputdata, varname_options, control_group = "all", event_baseperiod=-1, min_event=NULL, max_event=NULL){
+  
+  results = DiDe(inputdata=inputdata, varname_options=varname_options, control_group=control_group, event_baseperiod=event_baseperiod, min_event=min_event, max_event=max_event)
+  
+  return(results)
+}
+
+
---
+++ R/DiD_plots.R
@@ -0,0 +1,231 @@
+
+PlotDiDgt <- function(DiD_results, ci_factor = 1.96, max_jitter=0.05, lower_event=NULL, upper_event=NULL, colors=NULL){
+  # ci_factor = 1.96; max_jitter=0.05; lower_event=-5; upper_event=5
+  
+  # read the data
+  if(is.list(DiD_results)){
+    figdata_temp = copy(DiD_results$results_cohort) 
+  }
+  if(is.data.table(DiD_results)){
+    figdata_temp = copy(DiD_results) 
+  }
+  
+  # set up the time variables
+  all_calendartimes = figdata_temp[, sort(unique(CalendarTime))]
+  figdata_temp[, CalendarTime := as.numeric(CalendarTime)]
+  figdata_temp[, EventTime := CalendarTime - Cohort]
+  if(!is.null(lower_event)){
+    figdata_temp = figdata_temp[EventTime >= lower_event]
+  }
+  if(!is.null(upper_event)){
+    figdata_temp = figdata_temp[EventTime <= upper_event]
+  }
+  figdata_temp = figdata_temp[order(CalendarTime,Cohort)] 
+  
+  # set up jitter
+  cohorts = figdata_temp[, unique(Cohort) ] 
+  jitter_vals = seq(-max_jitter, max_jitter, length.out=length(cohorts))
+  for(ii in 1:length(jitter_vals)){
+    figdata_temp[Cohort==cohorts[ii], jitter_val := jitter_vals[ii]]
+  } 
+  figdata_temp[, CalendarTime_jitter := CalendarTime + jitter_val ] 
+  
+  # make ggplot
+  gg <- ggplot(aes( x = CalendarTime_jitter, y = ATTge, colour = factor(Cohort)), data = figdata_temp) +
+    labs(x = "Calendar Time", color = "Cohort", y ="") +
+    geom_point() + theme_bw(base_size = 20) +
+    geom_errorbar(aes(ymin = ATTge - ci_factor * ATTge_SE, ymax = ATTge + ci_factor * ATTge_SE), width=0.1) +
+    scale_x_continuous(breaks = all_calendartimes) +
+    scale_y_continuous(breaks = pretty_breaks()) +
+    geom_hline(yintercept=0, color="black", linetype="dashed") + 
+    theme(legend.position = "bottom")
+  
+  # set colors manually
+  if(!is.null(colors)){
+    if(length(cohorts) == length(colors)){
+      gg = gg + scale_color_manual(values=colors) 
+    }
+    if(length(cohorts) == length(colors)){
+      warning(sprintf("The provided colors could not be used, as there are %s colors and %s cohorts in the results.", length(cohorts), length(colors)))
+    }
+  }
+  
+  return(gg)
+  
+}
+
+PlotDiDge <- function(DiD_results, ci_factor = 1.96, max_jitter=0.05, lower_event=NULL, upper_event=NULL, colors=NULL){
+  # ci_factor = 1.96; max_jitter=0.05; lower_event=-5; upper_event=5
+  
+  # read the data
+  if(is.list(DiD_results)){
+    figdata_temp = copy(DiD_results$results_cohort) 
+  }
+  if(is.data.table(DiD_results)){
+    figdata_temp = copy(DiD_results) 
+  }
+  
+  # set up the time variables
+  all_events = figdata_temp[, sort(unique(EventTime))]
+  omitted_event_time = figdata_temp[,sort(unique(Preevent))]
+  figdata_temp = figdata_temp[EventTime != Preevent]
+  figdata_temp[, EventTime := as.numeric(EventTime)]
+  if(!is.null(lower_event)){
+    figdata_temp = figdata_temp[EventTime >= lower_event]
+  }
+  if(!is.null(upper_event)){
+    figdata_temp = figdata_temp[EventTime <= upper_event]
+  }
+  figdata_temp = figdata_temp[order(EventTime,Cohort)] 
+  
+  # set up jitter
+  cohorts = figdata_temp[, unique(Cohort) ] 
+  jitter_vals = seq(-max_jitter, max_jitter, length.out=length(cohorts))
+  for(ii in 1:length(jitter_vals)){
+    figdata_temp[Cohort==cohorts[ii], jitter_val := jitter_vals[ii]]
+  } 
+  figdata_temp[, EventTime_jitter := EventTime + jitter_val ] 
+  
+  # make ggplot
+  gg <- ggplot(aes( x = EventTime_jitter, y = ATTge, colour = factor(Cohort)), data = figdata_temp) +
+    labs(x = "Event Time", color = "Cohort", y ="") +
+    geom_point() + theme_bw(base_size = 20) +
+    geom_errorbar(aes(ymin = ATTge - ci_factor * ATTge_SE, ymax = ATTge + ci_factor * ATTge_SE), width=0.1) +
+    scale_x_continuous(breaks = all_events) +
+    scale_y_continuous(breaks = pretty_breaks()) +
+    geom_hline(yintercept=0, color="black", linetype="dashed") +
+    annotate(geom = "text", x = omitted_event_time, y = 0, label = "Normalized\nto Zero", size = 4) +
+    theme(legend.position = "bottom")
+  
+  # set colors manually
+  if(!is.null(colors)){
+    if(length(cohorts) == length(colors)){
+      gg = gg + scale_color_manual(values=colors) 
+    }
+    if(length(cohorts) == length(colors)){
+      warning(sprintf("The provided colors could not be used, as there are %s colors and %s cohorts in the results.", length(cohorts), length(colors)))
+    }
+  }
+  
+  return(gg)
+  
+}
+
+
+PlotDiDe <- function(DiD_results, ci_factor = 1.96, max_jitter=0.05, lower_event=NULL, upper_event=NULL, colors=NULL){
+  # ci_factor = 1.96; max_jitter=0.05; lower_event=-5; upper_event=5
+  
+  # prepare the data
+  if(is.list(DiD_results)){
+    figdata_temp = copy(DiD_results$results_average) 
+  }
+  if(is.data.table(DiD_results)){
+    figdata_temp = copy(DiD_results) 
+  }
+  
+  # set up the time variables
+  all_events = figdata_temp[, sort(unique(EventTime))]
+  omitted_event_time = figdata_temp[,sort(unique(Preevent))]
+  figdata_temp = figdata_temp[EventTime != Preevent]
+  figdata_temp[, EventTime := as.numeric(EventTime)]
+  figdata_temp = figdata_temp[ EventTime >= lower_event & EventTime <= upper_event]
+  figdata_temp = figdata_temp[order(EventTime)] 
+  
+  # make ggplot
+  gg <- ggplot(aes( x = EventTime, y = ATTe), data = figdata_temp) +
+    labs(x = "Event Time", y ="") +
+    geom_point() + theme_bw(base_size = 20) +
+    geom_errorbar(aes(ymin = ATTe - ci_factor * ATTe_SE, ymax = ATTe + ci_factor * ATTe_SE), width=0.1) +
+    scale_x_continuous(breaks = all_events) +
+    scale_y_continuous(breaks = pretty_breaks()) +
+    geom_hline(yintercept=0, color="black", linetype="dashed") +
+    annotate(geom = "text", x = omitted_event_time, y = 0, label = "Normalized\nto Zero", size = 4) +
+    theme(legend.position = "bottom")
+  
+  return(gg)
+  
+}
+
+#' Plot the results from the DiD estimation.
+#' 
+#' @param DiD_results Output from the DiD function.
+#' @param plot_type There are 3 options: default option is plot_type="ge" plots separate points for each cohort (g) and event time (e); option plot_type="gt" plots separate points for each cohort (g) and calendar time (t); and option plot_type="e" plots the average across cohorts for each event time (e).
+#' @param ci_factor This is the multiplier that converts a standard errors (SE) into a confidence interval (CI). That is, CI = estimate +/- ci_factor*SE. Default option is ci_factor = 1.96, which corresponds to a 95% CI. A natural alternative is ci_factor = 1.645, which corresponds to a 90% CI.
+#' @param max_jitter This jitters the points so that they do not overlap. If plot_type="ge" or plot_type="gt", there will be multiple points overlapping due to multiple cohorts, and max_jitter>0 will dodge the points by at most max_jitter. Default option is max_jitter=0.05.
+#' @param lower_event Minimum event time (e) to show in the plot. Default option is NULL.
+#' @param upper_event Minimum event time (e) to show in the plot. Default option is NULL.
+#' @param colors Manually set the colors for each cohort. Only relevant if plot_type="ge" or plot_type="gt". Default option is NULL.
+#' @returns A ggplot object.
+#' @examples
+#' PlotDiD(DiD_results, plot_type="ge")
+#' PlotDiD(DiD_results, plot_type="ge", lower_event=-3, upper_event=5, ci_factor = 1.96, max_jitter=0.05, colors=c("black","blue"))
+#' @export
+PlotDiD <- function(DiD_results, plot_type="ge", ci_factor = 1.96, max_jitter=0.05, lower_event=NULL, upper_event=NULL, colors=NULL){
+  gg = NULL
+  if(plot_type=="gt"){
+    gg = PlotDiDgt(DiD_results=DiD_results, lower_event=lower_event, upper_event=upper_event, ci_factor = ci_factor, max_jitter=max_jitter, colors=colors)
+  }
+  if(plot_type=="ge"){
+    gg = PlotDiDge(DiD_results=DiD_results, lower_event=lower_event, upper_event=upper_event, ci_factor = ci_factor, max_jitter=max_jitter, colors=colors)
+  }
+  if(plot_type=="e"){
+    gg = PlotDiDe(DiD_results=DiD_results, lower_event=lower_event, upper_event=upper_event, ci_factor = ci_factor, max_jitter=max_jitter, colors=colors)
+  }
+  return(gg)
+}
+
+
+
+
+
+PlotEventsgt <- function(DiD_results, cohort=2007, ci_factor = 1.96, max_jitter=0.05, colors=NULL){
+  # cohort=2007; ci_factor = 1.96; max_jitter=0.05; lower_event=-5; upper_event=5
+  
+  # prepare the data
+  if(is.list(DiD_results)){
+    figdata_temp = copy(DiD_results$results_cohort) 
+  }
+  if(is.data.table(DiD_results)){
+    figdata_temp = copy(DiD_results) 
+  }
+  all_calendartimes = figdata_temp[, sort(unique(CalendarTime))]
+  figdata_temp[, CalendarTime := as.numeric(CalendarTime)]
+  figdata_temp = figdata_temp[Cohort==cohort]
+  
+  figdata_temp = figdata_temp[,.(CalendarTime, Econtrol_post, Econtrol_SE, Etreated_post, Etreated_SE)]
+  figdata_temp1 = melt(figdata_temp[,.(CalendarTime, Econtrol_post, Etreated_post)], id.vars="CalendarTime")
+  figdata_temp1[, Group := "Control"]
+  figdata_temp1[variable=="Etreated_post", Group := "Treated"]
+  figdata_temp2 = melt(figdata_temp[,.(CalendarTime, Econtrol_SE, Etreated_SE)], id.vars="CalendarTime")
+  figdata_temp2[, Group := "Control"]
+  figdata_temp2[variable=="Etreated_SE", Group := "Treated"]
+  figdata_temp1 = figdata_temp1[,list(CalendarTime, Group, Mean=value)]
+  figdata_temp2 = figdata_temp2[,list(CalendarTime, Group, SE=value)]
+  figdata_temp3 = merge(figdata_temp1, figdata_temp2, by=c("CalendarTime","Group"))
+  
+  groups = figdata_temp3[, unique(Group) ] 
+  jitter_vals = seq(-max_jitter, max_jitter, length.out=length(groups))
+  for(ii in 1:length(jitter_vals)){
+    figdata_temp3[Group==groups[ii], jitter_val := jitter_vals[ii]]
+  } 
+  figdata_temp3[, CalendarTime_jitter := CalendarTime + jitter_val ] 
+  
+  gg <- ggplot(aes( x = CalendarTime_jitter, y = Mean, colour = factor(Group)), data = figdata_temp3) +
+    labs(x = "Calendar Time", color = "", y ="") +
+    geom_point() + theme_bw(base_size = 20) +
+    geom_errorbar(aes(ymin = Mean - ci_factor * SE, ymax = Mean + ci_factor * SE), width=0.1) +
+    scale_x_continuous(breaks = all_calendartimes) +
+    scale_y_continuous(breaks = pretty_breaks()) +
+    geom_vline(xintercept=cohort, color="black", linetype="dashed") +
+    # annotate(geom = "text", x = omitted_event_time, y = 0, label = "Normalized\nto Zero", size = 4) +
+    theme(legend.position = "bottom")
+  
+  if(!is.null(colors)){
+    gg = gg + scale_color_manual(values=colors) 
+  }
+  
+  return(gg)
+  
+}
+
+
