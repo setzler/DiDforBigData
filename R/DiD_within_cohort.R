b6108394f3d463839645cf3168a1bb94ba1309cb
Bradley Setzler <setzler@users.noreply.github.com>
2024-06-15 15:38:45 -0400

    Fix bug in fixedeffect_names argument

--- R/DiD_within_cohort.R
+++ R/DiD_within_cohort.R
@@ -155,7 +155,7 @@ DiDge_main <- function(inputdata, varnames, cohort_time, event_postperiod, base_
     if(return_data){
       data_prepost[, Cohort := cohort_time]
       data_prepost[, EventTime := event_postperiod]
-      export_vars = c(id_name,"Cohort","EventTime","treated",paste0(keep_vars,"_diff"),cluster_names)
+      export_vars = c(id_name,"Cohort","EventTime","treated",paste0(keep_vars,"_diff"),cluster_names,fixedeffect_names)
       data_prepost = data_prepost[,.SD,.SDcols=export_vars]
       return(list(results=results,data_prepost=data_prepost))
     }
